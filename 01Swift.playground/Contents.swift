51a08bd69cf6f4a37ffedb24ba580002ee8cfeb7
guochao <>
2020-12-11 10:24:40 +0800

    2020-12-11 10:24:38

--- 01Swift.playground/Contents.swift
+++ 01Swift.playground/Contents.swift
@@ -42,6 +42,84 @@
 //PlaygroundPage.current.liveView = navgetionc
 
 import Foundation
+//
+//let  a = 8
+//let b :UInt8 = 253
+//print("uint8 :\(UInt8.min), \(UInt8.max)")
+//
+//let a = 1
+//
+//if a == 1 {
+//    print("a")
+//}
+
+//typealias AutoString = String
+//
+//let a:AutoString = "a"
+//print(a)
+
+//let  x = 0.0, y = 0.0, z = 0.0
+//let  x = 0.0, y = 0.0, z = "123"
+//print("\(x), \(y), \(z)")
+
+//let error = (1, "error")
+//let (errorCode, errorMessage) = error
+//print(error.0)
+//print(error.1)
+//print(errorCode)
+//print(errorMessage)
+
+//let error = (errorCode:1, errorMessage:"error")
+//print(error.errorCode)
+//print(error.errorMessage)
+
+//var error:(errorCode: Int, errorMessage: Any) = (errorCode:1, errorMessage:"error")
+//
+//error.errorMessage = 1
+//print(error)
+
+//
+//func writeFile(str: String) -> (errorCode: Int, errorMessage: String){
+//    return (1, "error")
+//}
+//
+//let e = writeFile(str: "")
+//print(e)
+
+//let str: String? = "abc"
+//if str != nil {
+//    let  count = str!.count
+//    print(count)
+//}
+
+//if let a = str {
+//    let count = a.count
+//    print(count)
+//
+//}
+
+//let str: String? = nil
+//let count = str!.count
+
+let str: String! = ""
+let count = str.count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
