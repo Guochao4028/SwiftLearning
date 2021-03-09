81a295ec2de69d4249c86a5185b16d6caca1cba3
guochao <>
2021-03-09 16:27:20 +0800

    2021-03-09 16:27:18

--- 01Swift.playground/Pages/函数式编程.xcplaygroundpage/Contents.swift
+++ 01Swift.playground/Pages/函数式编程.xcplaygroundpage/Contents.swift
@@ -1,7 +1,100 @@
-//: [Previous](@previous)
+
 
 import Foundation
 
-var str = "Hello, playground"
+//var arr = [1, 2, 3, 5]
 
-//: [Next](@next)
+//var  arr2 = arr.map{$0 * 2}
+//
+//var arr3 = arr.filter{ $0 % 2 == 0}
+//
+//print(arr2)
+//print(arr3)
+
+
+//var arr3 = arr.reduce(0) { (result, element) -> Int in
+//    return result + element
+//}
+//
+//print(arr3)
+
+//var arr = Array.init(repeating: 0, count: 5)
+//
+//
+//var arr1 = arr.flatMap {
+//
+//    Array.init(repeating: $0, count: 1)
+//}
+//
+//print(arr1)
+
+//let arr = [1,2,3]
+//let result = arr.lazy.map { (i: Int) -> Int in
+//    return i * 2
+//}
+
+//optional 的map 和 flatMap
+
+
+
+// 假设 要实现以下功能 【（num + 3) * 5 -1】%10/2
+
+//func jia(_ v1: Int, _ v2: Int) -> Int{
+//    v1 + v2
+//}
+//
+//func cheng(_ v1: Int, _ v2: Int) -> Int{
+//    v1 * v2
+//}
+//
+//func jian(_ v1: Int, _ v2: Int) -> Int{
+//    v1 - v2
+//}
+//
+//func yu(_ v1: Int, _ v2: Int) -> Int{
+//    v1 % v2
+//}
+//
+//func chu(_ v1: Int, _ v2: Int) -> Int{
+//    v1 / v2
+//}
+//
+let num = 1
+//print(chu(yu(jian(cheng(jia(num, 3), 5), 1), 10), 2))
+
+typealias FN = (Int) -> Int
+
+func jia(_ v: Int) -> (Int) -> Int{ {$0 + v} }
+func cheng(_ v: Int) -> (Int) -> Int{ {$0 * v} }
+func jian(_ v: Int) -> (Int) -> Int{ {$0 - v} }
+func yu(_ v: Int) -> (Int) -> Int{ {$0 % v} }
+func chu(_ v: Int) -> (Int) -> Int{ {$0 / v} }
+//
+let fun1 = jia(3)
+let fun2 = cheng(5)
+let fun3 = jian(1)
+let fun4 = yu(10)
+let fun5 = chu(2)
+//
+//print(fun5(fun4(fun3(fun2(fun1(num))))))
+
+
+infix operator >> : AdditionPrecedence
+
+//B 是中间过程， A是入口，C 是结果
+func >> <A, B, C> (_ f1: @escaping (A) -> B,
+                   _ f2: @escaping (B) -> C )
+                    -> (A) -> C {
+    {
+        return f2(f1($0))
+    }
+}
+
+let fn = fun1 >> fun2 >> fun3 >> fun4 >> fun5
+print(fn(num))
+
+
+
+
+
+
--- TestSwift/TestSwift/main.swift
+++ TestSwift/TestSwift/main.swift
@@ -56,24 +56,26 @@ import Foundation
 //}
 
 
-
-func getA(_ v1: Int, _ v2: @autoclosure () -> Int )->Int{
-    print("*********************8")
-    return v1 > 0 ? v1 : v2()
-}
-
-func getA(_ v1: Int, _ v2: () -> Int )->Int{
-    print("____________________")
-    return v1 > 0 ? v1 : v2()
-}
-
-print(getA(-10, { () -> Int in
-    let a = 10
-    let b = 20
-    return a + b
-}()))
-
-print(getA(10){
-    return 30
-})
+//
+//func getA(_ v1: Int, _ v2: @autoclosure () -> Int )->Int{
+//    print("*********************8")
+//    return v1 > 0 ? v1 : v2()
+//}
+//
+//func getA(_ v1: Int, _ v2: () -> Int )->Int{
+//    print("____________________")
+//    return v1 > 0 ? v1 : v2()
+//}
+//
+//print(getA(-10, { () -> Int in
+//    let a = 10
+//    let b = 20
+//    return a + b
+//}()))
+//
+//print(getA(10){
+//    return 30
+//})
  
+
+infix operator ~>
